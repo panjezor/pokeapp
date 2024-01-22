1f12c4c6bd914a6bef14215198b158b2c5fbb96a
panjezor <filipjancieslik@gmail.com>
2024-01-22 17:25:41 +0000

    basic setup of Filament and Pint

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
 
@@ -167,6 +169,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\Filament\PokePanelProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
 
